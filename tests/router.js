7ba3e14e331153f65baa2b11bd3a3e405601cab7
johnhof <john.hofrichter@gmail.com>
2017-01-13 11:14:24 -0800

    finished controller tests

--- tests/router.js
+++ tests/router.js
@@ -24,15 +24,19 @@ describe('Router', () => {
   });
 
   describe('Controllers', () => {
-    it('should take a controllers object and return a middleware function', () => {
+    it('should take a controllers object and return a middleware function', (done) => {
       let router = new Router();
       expect(router).instanceof(Router);
-      router.controllers({});
-
-      //
-      // TODO: test
-      //
-
+      let called = false;
+      router.controllers({ controller: { baz: {
+        get: (ctx, next) => {
+          called = true;
+          return next();
+        }
+      } } })(CTX.get('/baz').context(SWAGGER.paths['/baz/list'].get), (ctx) => {
+        if (called) done();
+        else done('Failed to route');
+      });
     });
     it('should take a controllers directory and return a middleware function', (done) => {
       let router = new Router();
@@ -43,56 +47,52 @@ describe('Router', () => {
       router.controllers(ctrlDir)({}, doneCondition);
       router.controllers({ path: ctrlDir })({}, doneCondition);
     });
-    it('should reject invalid parameters', (done) => {
+    it('should reject invalid parameters', () => {
       let router = new Router();
       expect(() => router.controllers()).to.throw('requires');
       expect(() => router.controllers('asdf')).to.throw('ENOENT');
       expect(() => router.controllers({ path: 'asdf' })).to.throw('ENOENT');
     });
     describe('Controllers Middleware', () => {
+      let router = new Router();
+      let ctrlDir = `${__dirname}/controllers`;
+
       it('should route to flat controller methods', (done) => {
-        let router = new Router();
-        expect(router).instanceof(Router);
-        let ctrlDir = `${__dirname}/controllers`;
         router.controllers(ctrlDir)(CTX.get('/baz').context(SWAGGER.paths['/baz/list'].get), (ctx) => {
-          if (ctx.called('baz.get')) done();
+          if (ctx.called == 'baz.get') done();
           else done('Failed to route');
         });
       });
-      it('should route to nested controller methods specified by tag', () => {
-
-        //
-        // TODO: test
-        //
-
+      it('should route to nested controller methods specified by tag', (done) => {
+        router.controllers(ctrlDir)(CTX.get('/foo/bar/list').context(SWAGGER.paths['/foo/bar/list'].get), (ctx) => {
+          if (ctx.called == 'foo.bar.get') done();
+          else done('Failed to route');
+        });
       });
-      it('should route to nested controller methods by tag ignoring unrecognized tags', () => {
-
-        //
-        // TODO: test
-        //
-
+      it('should route to nested controller methods by tag stopping at unrecognized tags', (done) => {
+        router.controllers(ctrlDir)(CTX.get('/foo/bar/list').context(SWAGGER.paths['/foo/bar/list'].get), (ctx) => {
+          if (ctx.called == 'foo.bar.get') done();
+          else done('Failed to route');
+        });
       });
-      it('should route to by operationId', () => {
-
-        //
-        // TODO: test
-        //
-
+      it('should route to by operationId', (done) => {
+        router.controllers(ctrlDir)(CTX.get('/foo/bar/search').context(SWAGGER.paths['/foo/bar/search'].get), (ctx) => {
+          if (ctx.called == 'searchFooBar') done();
+          else done('Failed to route');
+        });
       });
-      it('should prioritize operationId over controller namespace', () => {
-
-        //
-        // TODO: test
-        //
-
+      //
+      it('should prioritize operationId over controller namespace', (done) => {
+        router.controllers(ctrlDir)(CTX.get('/foo/bar/search').context(SWAGGER.paths['/foo/bar/search'].get), (ctx) => {
+          if (ctx.called == 'searchFooBar') done();
+          else done('Failed to route');
+        });
       });
-      it('should forward unrecognized routes', () => {
-
-        //
-        // TODO: test
-        //
-
+      it('should forward unrecognized routes', (done) => {
+        router.controllers(ctrlDir)(CTX.get('/foo/bar/holla').context({}), (ctx) => {
+          if (!(ctx && ctx.called)) done();
+          else done(`routed to ${ctx.called}`);
+        });
       });
     });
   });
@@ -125,7 +125,7 @@ describe('Router', () => {
     it('should return middleare which bypasses the function when the tag is not present', (done) => {
       let router = new Router();
       let exec = false;
-      let middleware = router.tag('authenticated', (ctx, next) => { console.log('wtf'); exec = true; return next(); });
+      let middleware = router.tag('authenticated', (ctx, next) => { exec = true; return next(); });
       let ctx = CTX.get('/foo/me').context(SWAGGER.paths['/foo/{id}'].update)
       middleware(ctx, () => { expect(exec).to.be.false; done(); });
     });
